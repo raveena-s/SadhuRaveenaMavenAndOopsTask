737366c95b4ca98fe20e872ecff08b0bad8d5322
ravs <ravs@LAPTOP-5A97DHFV>
2020-07-24 12:35:40 +0530

    Epam assignment on maven and oops

---
+++ MavenAndOopsEclipsePrj/src/main/java/MavenAndOops/MavenAndOopsEclipsePrj/Cadbury.java
@@ -0,0 +1,12 @@
+package MavenAndOops.MavenAndOopsEclipsePrj;
+
+public class Cadbury extends Chocolate 
+{
+	public void inputChocolate()
+	{
+		System.out.println("Enter weight of Cadbury Chocolate:");
+		weight=scan.nextInt();
+		System.out.println("Enter price of Cadbury Chocolate:");
+		price=scan.nextInt();	
+	}
+}
